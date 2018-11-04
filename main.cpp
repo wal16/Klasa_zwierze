085183b22798f078586b9c417b8754b2e291bd64
Waldemar Mozola <waldemar.mozola@onet.pl>
2018-11-04 01:20:21 +0100

    dwie metody dodano

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Zwierze {
+public:
+
+    string gatunek;
+    string imie;
+    int wiek;
+
+    void dodaj_zwierze() {
+        cout << "Dodawanie nowego zwierzia do bazy: " << endl;
+        cout << "Podaj gatunek: ";
+        cin >> gatunek;
+        cout << "Podaj imie: ";
+        cin >> imie;
+        cout << "Podaj wiek: ";
+        cin >> wiek;
+    }
+
+    void daj_glos() {
+        if (gatunek == "kot") cout << imie << " lat" << wiek << ": Miau!";
+        else if (gatunek == "koza") cout << imie << " lat" << wiek << ": Beee!";
+        else if (gatunek == "krowa") cout << imie << " lat" << wiek << ": Muuu!";
+        else cout << "Nieznany gatunek! Moze jakies UFO?!";
+    }
+};
+
+int main() {
+    Zwierze z1;
+    z1.dodaj_zwierze();
+    z1.daj_glos();
+
+    Zwierze z2;
+    z2.dodaj_zwierze();
+    z2.daj_glos();
+
+    return 0;
+}
